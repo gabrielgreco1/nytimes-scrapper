5a607d9d4cfc6defe43e20b7f2313275d645f933
gabrielgreco1 <gabrielargreco@gmail.com>
2024-03-28 11:53:50 -0300

    Implementing robocorp changes

--- infra/driver.py
+++ infra/driver.py
@@ -5,14 +5,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from robocorp.tasks import task
 
 def driverSettings():
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument('--disable-web-security')
-    # chrome_options.add_argument("--start-maximized")
-    # chrome_options.add_experimental_option("excludeSwitches", ["disable-logging"])
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument('--disable-web-security')
+    options.add_argument("--start-maximized")
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    driver = webdriver.Chrome(service=None, options=chrome_options)
+    driver = webdriver.Chrome(service=None, options=options)
     return driver
