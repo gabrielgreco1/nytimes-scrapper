b7e859e753b95084e5cba9dea8eb51bb032621b6
gabrielgreco1 <gabrielargreco@gmail.com>
2024-03-30 02:16:15 -0300

    robocorp test

--- tasks.py
+++ tasks.py
@@ -2,10 +2,9 @@ from robocorp.tasks import task
 from app.run import RunClass
 from infra.driver import driver
 import config
-
+driver_class = driver()
+driver1 = driver_class.set_webdriver()
+ny_search = RunClass(driver1, config.query, config.subject)
 @task
 def run_task():
-    driver_class = driver()
-    driver1 = driver_class.set_webdriver()
-    ny_search = RunClass(driver1, config.query, config.subject)
     ny_search.run_search()
