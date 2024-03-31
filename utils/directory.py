31498ee80610cbccf08073851ffcc94f50a17abd
gabrielgreco1 <gabrielargreco@gmail.com>
2024-03-30 21:57:51 -0300

    testing directory

--- utils/directory.py
+++ utils/directory.py
@@ -13,10 +13,10 @@ class directory:
         if not os.path.exists(directory_path):
             os.makedirs(directory_path)
 
-        print(f"Artifacts directory: {directory_path.absolute()}")
+        print(f"Artifacts directory: {directory_path}")
 
         # Verifica se o diretório existe e cria se não existir.
         if not directory_path.exists():
-            print(f"Directory {directory_path.absolute()} does not exist. Creating it.")
+            print(f"Directory {directory_path} does not exist. Creating it.")
             directory_path.mkdir(parents=True, exist_ok=True)
 
