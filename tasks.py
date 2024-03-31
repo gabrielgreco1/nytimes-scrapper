ab60babbd61e66c673027b2fefc6b070d280b852
gabrielgreco1 <gabrielargreco@gmail.com>
2024-03-31 18:57:08 -0300

    feat: implementing workitem

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from app.run import RunClass
 from infra.driver import driver
 from robocorp.tasks import task
+from robocorp import workitems
 import config
 
 driver_class = driver()
@@ -10,3 +11,6 @@ ny_search = RunClass(driver1, config.query, config.subject)
 @task
 def run_task():
     ny_search.run_search()
+    item = workitems.inputs.current
+    print("Received payload:", item.payload)
+    workitems.outputs.create(payload={"key": "value"})
